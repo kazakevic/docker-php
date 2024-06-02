3ee8427efde6027a80c00d926796bb2eeb084943
Darjus <darjus.kazakevic@gmail.com>
2024-06-02 15:04:47 +0300

    chore: gitignore

--- app/src/Controller/HealthCheckController.php
+++ app/src/Controller/HealthCheckController.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HealthCheckController 
+class HealthCheckController
 {
     #[Route('/api/health', name: 'health_check', methods: ['post'])]
     public function __invoke(): Response
